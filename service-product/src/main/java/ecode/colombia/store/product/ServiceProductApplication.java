a440d4fdd9686c36ef12da9002918762c75984c7
Jhon Garcia <jhon3724@gmail.com>
2022-10-24 22:04:04 -0500

    update services with eureka

--- service-product/src/main/java/ecode/colombia/store/product/ServiceProductApplication.java
+++ service-product/src/main/java/ecode/colombia/store/product/ServiceProductApplication.java
@@ -2,7 +2,9 @@ package ecode.colombia.store.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ServiceProductApplication {
 
