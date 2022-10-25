361cfe7d44dc79ee65f64783be626640935e0671
Jhon Garcia <jhon3724@gmail.com>
2022-10-24 19:59:15 -0500

    Shopping service

---
+++ shopping-service/src/main/java/ecode/colombia/store/shopping/repository/InvoiceItemsRepository.java
@@ -0,0 +1,7 @@
+package ecode.colombia.store.shopping.repository;
+
+import ecode.colombia.store.shopping.entity.InvoiceItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvoiceItemsRepository extends JpaRepository<InvoiceItem, Long> {
+}
---
+++ shopping-service/src/main/java/ecode/colombia/store/shopping/service/InvoiceService.java
@@ -0,0 +1,13 @@
+package ecode.colombia.store.shopping.service;
+
+import ecode.colombia.store.shopping.entity.Invoice;
+
+import java.util.List;
+
+public interface InvoiceService {
+    public List<Invoice> findInvoiceAll();
+    public Invoice createInvoice(Invoice invoice);
+    public Invoice updateInvoice(Invoice invoice);
+    public Invoice deleteInvoice(Invoice invoice);
+    public Invoice getInvoice(Long id);
+}
